97a12166e5e1abb7a7f33c2e44e2e3179c984200
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-05 01:42:16 +0800

    added maincontent and hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import { twMerge } from "tailwind-merge";
+
+const Hero = () => {
+  return (
+    <div
+      id="home"
+      className={twMerge(
+        "min-h-screen min-h-dvh max-w-5xl mx-auto flex flex-col justify-center",
+      )}
+    >
+      <div className="w-full max-w-xl">
+          <h1 className="text-4xl md:text-5xl font-bold pb-2">
+            Hi, I'm Kurt Vincent Almodovar
+          </h1>
+          <p className="text-base md:text-xl text-gray-500 pb-4">
+            Software Developer & Data Engineer
+          </p>
+          <p className="text-lg">
+            Creating web apps and analyzing data to solve real-world problems
+            based in <strong>Santa Rosa City, Laguna.</strong>
+          </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/MainContent.tsx
@@ -0,0 +1,13 @@
+import Hero from "@/components/Hero"
+import Skills from "@/components/Skills"
+
+const MainContent = () => {
+  return (
+    <main className="px-4 py-4">
+        <Hero/>
+        <Skills/>
+    </main>
+  )
+}
+
+export default MainContent
