2923415e3cd60ec49ab5f02f8b1b6f1e8633b3bb
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-04 22:30:00 +0800

    add Languages and Skills components with tech stack data

---
+++ src/components/Languages.tsx
@@ -0,0 +1,34 @@
+import techStack from "@/data/techStack";
+
+const Languages = () => {
+  const { languages } = techStack;
+
+  return (
+    <>
+      <h3>Languages</h3>
+      <ul className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-7 gap-2">
+        {languages.map((language, index) => {
+          return (
+            <li key={index}>
+              <div className="flex flex-col items-center shadow-md border-1 border-gray rounded-md p-8">
+                <svg
+                  className="h-16 w-16"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox={language.svg.viewBox}
+                >
+                  {language.svg.path.map((icon, pathIndex) => {
+                    return <path key={pathIndex} d={icon} />;
+                  })}
+                </svg>
+                <p className="font-bold">{language.name}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Languages;
---
+++ src/components/Skills.tsx
@@ -0,0 +1,19 @@
+import Languages from "@/components/Languages";
+
+const Skills = () => {
+  return (
+    <section className="max-w-5xl mx-auto">
+      {
+        // • Languages: PHP, Python, JavaScript, HTML5, CSS3, SQL, Java, C++, R
+        // • Frameworks & Libraries: Laravel, Flask, React, Tailwind CSS, Shiny
+        // • Databases: MySQL, SQLite
+        // • Tools & Platforms: Git, GitHub, Docker, Jira, Confluence, Figma, Google Search Console
+      }
+
+      <h2 className="text-3xl md:text-4xl font-bold pb-2 flex-">Tech Stack</h2>
+      <Languages />
+    </section>
+  );
+};
+
+export default Skills;
