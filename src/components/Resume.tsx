1ebbb260d7b00ab83e252d9ffd119c27416aadaa
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-06 02:42:45 +0800

    updated resume component content & added sr-only class on h2 for accessibility

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/Button";
 import {
   Card,
+  CardDescription,
   CardContent,
   CardFooter,
   CardHeader,
@@ -16,17 +17,19 @@ const Resume = () => {
       className="text-center min-h-screen flex justify-center items-center"
     >
       <div className="">
-        <h2 className="text-3xl text-center md:text-4xl font-bold pb-4">
+        <h2 className="sr-only">
           Resume
         </h2>
         <Card className="mx-auto w-full h-full max-w-md">
           <CardHeader>
-            <CardTitle className="text-xl">Software Developer</CardTitle>
+            <CardTitle>Software Developer</CardTitle>
+            <CardDescription>
+              Student at Polytechnic University of the Philippines
+            </CardDescription>
           </CardHeader>
-          <CardContent className="text-foreground/70">
+          <CardContent>
             With hands-on experience building full-stack and backend web
-            applications. Focused on writing clean, scalable code and delivering
-            solutions that solve real-world problems.
+            applications.
           </CardContent>
           <CardFooter>
             <Button variant="outline" asChild className="w-full">
@@ -34,7 +37,7 @@ const Resume = () => {
                 href="https://drive.google.com/drive/folders/1o1dGH22KkmVKjPmAaL89Ova3y_s22hWg?usp=sharing"
                 target="_blank"
               >
-                View
+                View Resume
                 <SquareArrowOutUpRight />
               </a>
             </Button>
