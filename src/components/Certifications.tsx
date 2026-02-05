a7b299e1eb2cdc3a037d11d69f68b7534c83db07
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-05 18:11:30 +0800

    improved responsiveness of carousel & add offset for scroll navigation

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -12,13 +12,13 @@ import SectionTemplate from "@/components/sections/SectionTemplate";
 
 const Certifications = () => {
   return (
-    <SectionTemplate className="max-w-full">
+    <SectionTemplate id="certifications">
       <h2 className="text-3xl md:text-4xl font-bold pb-4">Certifications</h2>
 
-      <Carousel className="w-full max-w-[12rem] sm:max-w-xs md:max-w-5xl">
+      <Carousel className="w-full md:max-w-xs md:max-w-5xl">
         <CarouselContent className="-ml-1">
           {certificates.map((certificate, index) => (
-            <CarouselItem key={index} className="basis-1/2 pl-1 lg:basis-1/2">
+            <CarouselItem key={index} className="basis-1/1 pl-1 md:basis-1/2">
               <div className="p-1">
                 <Card className="py-0 overflow-hidden">
                   <CardContent className="relative flex aspect-video items-center justify-center p-6">
@@ -33,8 +33,10 @@ const Certifications = () => {
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+        <div className="flex justify-center gap-4 mt-4">
+          <CarouselPrevious className="static translate-y-0" />
+          <CarouselNext className="static translate-y-0" />
+        </div>
       </Carousel>
     </SectionTemplate>
   );
