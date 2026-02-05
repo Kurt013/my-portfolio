434db8551c731dc2bdcdbbb522d69a6448c9c76b
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-05 15:55:10 +0800

    updated hero section info

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,21 +5,20 @@ const Hero = () => {
   return (
     <SectionTemplate
       id="home"
-      className={cn(
-        "min-h-screen min-h-dvh flex flex-col justify-center"
-      )}
+      className={cn("min-h-screen min-h-dvh flex flex-col justify-center")}
     >
       <div className="w-full max-w-xl">
-          <h1 className="text-4xl md:text-5xl font-bold pb-2">
-            Hi, I'm Kurt Vincent Almodovar
-          </h1>
-          <p className="text-base md:text-xl text-gray-500 pb-4">
-            Software Developer & Data Engineer
-          </p>
-          <p className="text-lg">
-            Creating web apps and analyzing data to solve real-world problems
-            based in <strong>Santa Rosa City, Laguna.</strong>
-          </p>
+        <h1 className="text-4xl md:text-5xl font-bold pb-2">
+          Hi, I'm Kurt Vincent Almodovar
+        </h1>
+        <p className="text-base md:text-xl text-gray-500 pb-4">
+          Software Developer
+        </p>
+        <p className="text-lg">
+          Building reliable applications across
+          backend and frontend, based in{" "}
+          <strong>Santa Rosa City, Laguna</strong>.
+        </p>
       </div>
     </SectionTemplate>
   );
