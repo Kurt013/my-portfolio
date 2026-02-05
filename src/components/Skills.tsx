13c8027f8f5face949a89432e573f1401c1bd5e9
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-06 02:35:30 +0800

    updated tech stack data

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,13 +7,6 @@ import SectionTemplate from "./sections/SectionTemplate";
 const Skills = () => {
   return (
     <SectionTemplate id="skills">
-      {
-        // • Languages: PHP, Python, JavaScript, HTML5, CSS3, SQL, Java, C++, R
-        // • Frameworks & Libraries: Laravel, Flask, React, Tailwind CSS, Shiny
-        // • Databases: MySQL, SQLite
-        // • Tools & Platforms: Git, GitHub, Docker, Jira, Confluence, Figma, Google Search Console
-      }
-
       <h2 className="text-3xl md:text-4xl font-bold pb-4">Tech Stack</h2>
       <Languages />
       <FrameworksLibraries />
