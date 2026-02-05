0e9b0cbf9c768ab0968bec827169fca771c6b03c
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-05 14:31:33 +0800

    added themeprovider component to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import HeaderNav from '@/components/HeaderNav'
-import MainContent from '@/components/MainContent'
+import { ThemeProvider } from "@/components/ThemeProvider";
+import HeaderNav from "@/components/HeaderNav";
+import MainContent from "@/components/MainContent";
 
 function App() {
-  const navItems = ['Home', 'Projects', 'Certifications', 'Resume', 'Contact']
+  const navItems = ["Home", "Projects", "Certifications", "Resume", "Contact"];
 
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <HeaderNav navItems={navItems} />
-      <MainContent/>
-    </>
-  )
+      <MainContent />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
