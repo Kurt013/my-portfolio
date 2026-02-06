b4b847157428b541d698fb7a70633f12e9d0d091
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-06 22:33:55 +0800

    added github repo redirect on card components

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,6 +2,7 @@ const projects = [
   {
     githubThumbnail:
       "https://opengraph.githubassets.com/7901ff2dd93181d488db869a833da5f1a331bdf6372aed391126353cf71b5b1b/ayumihidalgo/E-skolarian",
+    githubLink: "https://github.com/ayumihidalgo/E-skolarian",
     projectName: "E-skolarian",
     role: "Frontend Developer",
     description:
@@ -11,6 +12,7 @@ const projects = [
   {
     githubThumbnail:
       "https://opengraph.githubassets.com/5a700a9f28da9bbff08ff1f3251460b6df3607233715791462dc576b8555fc33/Kurt013/Alignify-ChromeExtension",
+    githubLink: "https://github.com/Kurt013/Alignify-ChromeExtension",
     projectName: "Alignify",
     role: "",
     description: "A posture corrector for long sitting sessions",
@@ -25,6 +27,7 @@ const projects = [
   {
     githubThumbnail:
       "https://opengraph.githubassets.com/3ecbef62bfbde04c31342226734e843130128f46c2e3b713e1de636e62c968c6/Kurt013/Villa-Gilda-Resort",
+    githubLink: "https://github.com/Kurt013/Villa-Gilda-Resort",
     projectName: "Web-Based Reservation System",
     role: "Frontend Developer",
     description: "A web-based reservation system for Villa Gilda Resort",
@@ -33,6 +36,7 @@ const projects = [
   {
     githubThumbnail:
       "https://opengraph.githubassets.com/71a2e15730bf9eb462b71a07b5d2a33d01f0e709b0f0f10d0491931b6f7897fb/Kurt013/gacha-pull-logger",
+    githubLink: "https://github.com/Kurt013/gacha-pull-logger",
     projectName: "Gacha Pull Logger",
     role: "",
     description: "A fan-made gacha pull logging and analytics web application",
@@ -41,6 +45,7 @@ const projects = [
   {
     githubThumbnail:
       "https://opengraph.githubassets.com/877cd249c9eae33509cbd1c0649ec52e443f3cf685371cc38095f5a520350ee6/Kurt013/sinalhan",
+    githubLink: "https://github.com/Kurt013/sinalhan",
     projectName: "Document Issuance System",
     role: "Backend Developer",
     description: "A web-based document issuance system with QR code technology",
