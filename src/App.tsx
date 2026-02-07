fba023588c3c6767dbde50c2d8be559edbe5c263
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-08 02:27:38 +0800

    added vercel web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import HeaderNav from "@/components/HeaderNav";
 import MainContent from "@/components/MainContent";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   const navItems = ["Home", "Projects", "Certifications", "Resume", "Contact"];
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Analytics />
+
       <HeaderNav navItems={navItems} />
       <MainContent />
       <Footer />
