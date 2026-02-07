3f2922195fb34331daeb5fd8847a57c7f5755f73
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-08 02:43:41 +0800

    added the appropriate version of web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,20 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import HeaderNav from "@/components/HeaderNav";
 import MainContent from "@/components/MainContent";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const navItems = ["Home", "Projects", "Certifications", "Resume", "Contact"];
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <HeaderNav navItems={navItems} />
-      <MainContent />
-      <Footer />
-    </ThemeProvider>
+    <>
+      <Analytics />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <HeaderNav navItems={navItems} />
+        <MainContent />
+        <Footer />
+      </ThemeProvider>
+    </>
   );
 }
 
