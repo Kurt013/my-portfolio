13d3e9533d36455e3d8cfc2da2fd706dad8eda8f
Almodovar, Kurt Vincent Almendral <121716283+Kurt013@users.noreply.github.com>
2026-02-08 02:41:14 +0800

    reverted back; removed web analytics (not working)

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,16 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import HeaderNav from "@/components/HeaderNav";
 import MainContent from "@/components/MainContent";
 import Footer from "@/components/Footer";
-import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   const navItems = ["Home", "Projects", "Certifications", "Resume", "Contact"];
 
   return (
-    <>
-      <Analytics />
-
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <HeaderNav navItems={navItems} />
-        <MainContent />
-        <Footer />
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <HeaderNav navItems={navItems} />
+      <MainContent />
+      <Footer />
+    </ThemeProvider>
   );
 }
 
